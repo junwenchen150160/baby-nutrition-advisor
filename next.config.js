5376d59ef9d7001f108ab66d887c19d5ea9b79b0
rocky669733 <junwenc138@gmail.com>
2025-05-26 14:02:40 +0800

    Fix Next.js config for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -10,14 +10,6 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/baby-nutrition-advisor' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/baby-nutrition-advisor/' : '',
-  webpack: (config, { dev, isServer }) => {
-    if (dev && isServer) {
-      config.cache = {
-        type: 'memory'
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
